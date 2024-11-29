558d0e664e5e6ed4f353c0a39888094f7a1f36d0
deslrey <2135232170@qq.com>
2024-11-29 10:58:01 +0800

    init: 初始化仓库提交

---
+++ src/main/java/org/deslre/DeslreShopBootApplication.java
@@ -0,0 +1,13 @@
+package org.deslre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeslreShopBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeslreShopBootApplication.class, args);
+	}
+
+}
