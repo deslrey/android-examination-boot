c652038c2c5dbce2d2270561d8fafb1d1171db58
deslrey <2135232170@qq.com>
2024-12-08 21:54:58 +0800

    feat: 创建商品实体类

---
+++ src/main/java/org/deslre/controller/BarcodesController.java
@@ -0,0 +1,20 @@
+package org.deslre.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ * 标准商品条码表 前端控制器
+ * </p>
+ *
+ * @author author
+ * @since 2024-12-08
+ */
+@Controller
+@RequestMapping("/barcodes")
+public class BarcodesController {
+
+}
---
+++ src/main/java/org/deslre/service/IBarcodesService.java
@@ -0,0 +1,16 @@
+package org.deslre.service;
+
+import org.deslre.entity.Barcodes;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 标准商品条码表 服务类
+ * </p>
+ *
+ * @author author
+ * @since 2024-12-08
+ */
+public interface IBarcodesService extends IService<Barcodes> {
+
+}
