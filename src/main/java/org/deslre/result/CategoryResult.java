0b6ef70498be9b8c751e72bd5f445c7327e19273
deslrey <2135232170@qq.com>
2024-12-19 10:30:16 +0800

    feat: 上传分类

---
+++ src/main/java/org/deslre/result/CategoryResult.java
@@ -0,0 +1,72 @@
+package org.deslre.result;
+
+/**
+ * ClassName: CategoryResult
+ * Description: TODO
+ * Author: Deslrey
+ * Date: 2024-12-19 10:12
+ * Version: 1.0
+ */
+public class CategoryResult {
+
+    public static final String ENGLISH = "英语";
+    public static final String ENGLISH_COLLEGE_ENGLISH = "大学英语";
+    public static final String ENGLISH_KAO_YAN = "考研";
+    public static final String ENGLISH_PROFESSIONAL_ENGLISH = "专业英语";
+    public static final String ENGLISH_PET = "PET";
+    public static final String ENGLISH_SELF_TEST_ENGLISH_II = "自考英语二";
+    public static final String ENGLISH_OTHER_ONE = "其他";
+    public static final String ENGLISH_TOEFL = "TOEFL";
+    public static final String ENGLISH_GMAT = "GMAT";
+    public static final String ENGLISH_GRE = "GRE";
+    public static final String ENGLISH_IELTS = "IELTS";
+    public static final String ENGLISH_KET = "KET";
+    public static final String ENGLISH_SAT = "SAT";
+    public static final String ENGLISH_BFC = "BFC";
+    public static final String ENGLISH_PTE = "PTE";
+    public static final String ENGLISH_TOEIC = "TOEIC";
+    public static final String ENGLISH_CEFR = "CEFR";
+    public static final String ENGLISH_OXFORD_EDITION = "牛津版";
+    public static final String ENGLISH_OTHER_TWO = "其他";
+    public static final String ENGLISH_GENERAL = "通用";
+    public static final String ENGLISH_NEW_CONCEPT = "新概念";
+//    public static final String ENGLISH = "牛津版";
+//    public static final String ENGLISH = "人教版";
+//    public static final String ENGLISH = "外研版";
+//    public static final String ENGLISH = "译林版";
+//    public static final String ENGLISH = "北师大";
+//    public static final String ENGLISH = "冀教版";
+//    public static final String ENGLISH = "其他";
+//    public static final String ENGLISH = "EF";
+//    public static final String ENGLISH = "RAZ";
+
+
+    public static final String JAPANESE = "日语";
+    public static final String JAPANESE_BASIS = "基础";
+
+    public static final String DEUTSCH = "德语";
+    public static final String DEUTSCH_BASIS = "基础";
+
+    public static final String KAZAKH = "哈萨克语";
+    public static final String KAZAKH_OLD_TEXT = "老文字";
+    public static final String KAZAKH_HAPIN = "哈拼";
+    public static final String KAZAKH_CYRILLIC_ALPHABET = "西里尔字母";
+
+    public static final String INDONESIAN = "印尼语";
+    public static final String INDONESIAN_BASIS = "基础";
+
+    public static final String CODE = "Code";
+    public static final String CODE_GENERAL = "通用";
+    public static final String CODE_KIDS = "少儿编程";
+    public static final String CODE_Python = "Python";
+    public static final String CODE_C_SUPER = "C++";
+    public static final String CODE_ARDUINO = "Arduino";
+    public static final String CODE_JavaScript = "JavaScript";
+    public static final String CODE_Java = "Java";
+    public static final String CODE_Linux = "Linux";
+    public static final String CODE_C_SUPER_SUPER = "C#";
+    public static final String CODE_SQL = "SQL";
+    public static final String CODE_AI = "AI";
+    public static final String CODE_GOLANG = "Golang";
+
+}
