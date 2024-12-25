c4573db60f2815731e08169ccbafcc55c417764a
deslrey <2135232170@qq.com>
2024-12-25 15:43:50 +0800

    feat: 添加全局静态字段

---
+++ src/main/java/org/deslre/utils/StaticUtil.java
@@ -0,0 +1,15 @@
+package org.deslre.utils;
+
+/**
+ * ClassName: StaticUtil
+ * Description: 全局共享字段
+ * Author: Deslrey
+ * Date: 2024-12-25 15:42
+ * Version: 1.0
+ */
+public class StaticUtil {
+
+    public static final boolean TRUE = true;
+    public static final boolean FALSE = true;
+
+}
