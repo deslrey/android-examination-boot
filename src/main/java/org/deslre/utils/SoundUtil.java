af0418bab769180b6124a8a745f60c2365ec2bba
deslrey <2135232170@qq.com>
2024-12-25 23:16:36 +0800

    feat: 完成每个语言发音的请求接口

--- src/main/java/org/deslre/utils/SoundUtil.java
+++ src/main/java/org/deslre/utils/SoundUtil.java
@@ -12,18 +12,18 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class SoundUtil {
 
-
-    private static final String URL = "https://dict.youdao.com/dictvoice?audio=";
-
-    public static final String BRITISH_PRONUNCIATION = "";
-
+    private static final String English = "https://dict.youdao.com/dictvoice?audio=";
+    private static final String Japanese = "https://dict.youdao.com/dictvoice?audio=";
+    private static final String German = "https://dict.youdao.com/dictvoice?audio=";
+    private static final String Kazakh = "https://dict.youdao.com/dictvoice?audio=";
+    private static final String Indonesia = "https://dict.youdao.com/dictvoice?audio=";
 
     public static String getAmericanPronunciation(String word) {
         if (StringUtil.isEmpty(word)) {
             log.warn("查询的美式发音的单词不能为空");
             return null;
         }
-        return URL + word + "&type=" + 0;
+        return English + word + "&type=" + 0;
     }
 
     public static String getBritishPronunciation(String word) {
@@ -31,8 +31,45 @@ public class SoundUtil {
             log.warn("查询的英式发音的单词不能为空");
             return null;
         }
-        return URL + word + "&type=" + 1;
+        return English + word + "&type=" + 1;
     }
 
+    public static String getJapan(String word) {
+        if (StringUtil.isEmpty(word)) {
+            log.warn("查询的日语发音的单词不能为空");
+            return null;
+        }
+        return Japanese + word + "&le=jap";
+    }
+
+    public static String getGerman(String word) {
+        if (StringUtil.isEmpty(word)) {
+            log.warn("查询的德语发音的单词不能为空");
+            return null;
+        }
+        return German + word + "&le=de";
+    }
+
+    public static String getKazakh(String word) {
+        if (StringUtil.isEmpty(word)) {
+            log.warn("查询的哈萨克语哈拼发音的单词不能为空");
+            return null;
+        }
+        return Kazakh + word + "&le=ru";
+    }
+
+    public static String getIndonesia(String word) {
+        if (StringUtil.isEmpty(word)) {
+            log.warn("查询的哈萨克语哈拼发音的单词不能为空");
+            return null;
+        }
+        return Indonesia + word + "&le=id";
+    }
+
+
+    public static void main(String[] args) {
+        String string = getJapan("てまえ");
+        System.out.println("string = " + string);
+    }
 
 }
